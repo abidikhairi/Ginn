72f848dc740534f2a4aaa9baad7db36084b44d00
Khairi Abidi <abidikhairi.1337@gmail.com>
2025-05-09 17:12:27 +0100

    download swissprot instructions dataset

---
+++ data/swissprot_instructions.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+from datasets import load_dataset
+
+
+def main(args):
+    dataset_id = 'khairi/swissprot-instructions'
+    save_dir = args.save_dir
+    
+    dataset = load_dataset(dataset_id)
+    
+    splits = ('train', 'validation', 'test')
+    
+    for split in splits:
+        dataset[split] = dataset[split].to_csv(f'{save_dir}/{split}.csv', index=False)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    
+    parser.add_argument('--save-dir', required=True, help='Save directory')
+    
+    args = parser.parse_args()
+    main(args)
