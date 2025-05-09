04020c8c0eaa26a2b0c388c2f2f8cefaaa0ee3b5
Khairi Abidi <abidikhairi.1337@gmail.com>
2025-05-09 19:44:02 +0100

    if directory does not exists, create it

--- data/swissprot_instructions.py
+++ data/swissprot_instructions.py
@@ -1,3 +1,4 @@
+import os
 from argparse import ArgumentParser
 from datasets import load_dataset
 
@@ -9,6 +10,9 @@ def main(args):
     dataset = load_dataset(dataset_id)
     
     splits = ('train', 'validation', 'test')
+    
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir, exist_ok=True)
     
     for split in splits:
         dataset[split] = dataset[split].to_csv(f'{save_dir}/{split}.csv', index=False)
--- data/uniref50.py
+++ data/uniref50.py
@@ -1,3 +1,4 @@
+import os
 from argparse import ArgumentParser
 from datasets import load_dataset
 
@@ -13,6 +14,9 @@ def main(args):
     dataset = dataset.filter(lambda example: len(example['sequence']) >= min_seq_len and len(example['sequence']) <= max_seq_len)
     
     splits = ('train', 'validation', 'test')
+    
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir, exist_ok=True)
     
     for split in splits:
         dataset[split] = dataset[split].to_csv(f'{save_dir}/{split}.csv', index=False)
