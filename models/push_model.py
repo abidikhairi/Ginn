eddcf7313894e3a4707d56faf138f99bac7ce15f
Khairi Abidi <abidikhairi.1337@gmail.com>
2025-05-09 18:02:37 +0100

    handle models and adapters seperately

--- models/push_model.py
+++ models/push_model.py
@@ -1,13 +1,29 @@
+# push_model.py
+import argparse
 from transformers import AutoModelForCausalLM
+from peft import PeftConfig, PeftModel
 
-def main():
-    repo_id = 'khairi/SmolLM2-135M-Instruct'
-    ckpt_path = "/ddn/data/fcit/jghazialharbi/experim/CP-Protein-LLM/checkpoint-1500"
+
+def main(args):
+    repo_id = args.repo_id
+    ckpt_path = args.ckpt_path    
     
-    model = AutoModelForCausalLM.from_pretrained(ckpt_path)
-    
-    model.push_to_hub(repo_id)
+    if not args.push_only_adapter:
+        model = AutoModelForCausalLM.from_pretrained(ckpt_path)
+        model.push_to_hub(repo_id)
+    else:
+        config = PeftConfig.from_pretrained(ckpt_path)
+        base_model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path)
+        model = PeftModel.from_pretrained(base_model, ckpt_path)
+        model.push_to_hub(repo_id)
     
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--repo-id', required=True, help="Model HF repository")
+    parser.add_argument('--ckpt-path', required=True, help="Checkpoint path")
+    parser.add_argument('--push-only-adapter', action='store_true', help="Push only the LoRA adapter")
+        
+    args = parser.parse_args()
+    main(args)
